8311353621b455e3b3d827f9fd2085ccc81ff4e7
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-04-11 16:26:55 +0200

    Add settings

---
+++ etc/config/Run2016/settings_HNWR_HEEP.py
@@ -0,0 +1,106 @@
+#############################################################
+########## General settings
+#############################################################
+
+# flag to be Tested
+flags = {
+    'passingVeto'   : '(passingVeto   == 1)',
+    'passingLoose'  : '(passingLoose  == 1)',
+    'passingMedium' : '(passingMedium == 1)',
+    'passingTight'  : '(passingTight  == 1)',
+    'passingTight94XV2' : '(passingTight94XV2 == 1)',
+    'passingHEEP'   : '(passingHEEP == 1)',
+    }
+baseOutDir = 'results/Val/2016/'
+
+#############################################################
+########## samples definition  - preparing the samples
+#############################################################
+### samples are defined in etc/inputs/tnpSampleDef.py
+### not: you can setup another sampleDef File in inputs
+import etc.inputs.tnpSampleDef as tnpSamples
+tnpTreeDir = 'tnpEleIDs'
+
+samplesDef = {
+    'data'   : tnpSamples.MySamples['2016DATA'].clone(),
+    'mcNom'  : tnpSamples.MySamples['2016DYLO1'].clone(),
+    'mcAlt'  : tnpSamples.MySamples['2016DYNLO1'].clone(),
+    'tagSel' : tnpSamples.MySamples['2016DYLO2'].clone(),
+}
+## can add data sample easily
+#samplesDef['data'].add_sample( tnpSamples.ICHEP2016['data_2016_runC_ele'] )
+#samplesDef['data'].add_sample( tnpSamples.ICHEP2016['data_2016_runD_ele'] )
+
+## some sample-based cuts... general cuts defined here after
+## require mcTruth on MC DY samples and additional cuts
+## all the samples MUST have different names (i.e. sample.name must be different for all)
+## if you need to use 2 times the same sample, then rename the second one
+#samplesDef['data'  ].set_cut('run >= 273726')
+if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_mcTruth()
+if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_mcTruth()
+if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_mcTruth()
+if not samplesDef['tagSel'] is None:
+    samplesDef['tagSel'].rename('mcAltSel_DY_madgraph_ele')
+    samplesDef['tagSel'].set_cut('tag_Ele_pt > 35')
+
+## set MC weight, simple way (use tree weight) 
+weightName = 'totWeight'
+if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_weight(weightName)
+if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_weight(weightName)
+if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_weight(weightName)
+
+#############################################################
+########## bining definition  [can be nD bining]
+#############################################################
+biningDef = [
+   { 'var' : 'el_sc_eta' , 'type': 'float', 'bins': [0.0, 1.4442, 1.566, 2.5] },
+   { 'var' : 'el_pt' , 'type': 'float', 'bins': [35., 50, 100, 200, 500] },
+]
+
+#############################################################
+########## Cuts definition for all samples
+#############################################################
+### cut
+cutBase   = 'tag_Ele_pt > 30 && abs(tag_sc_eta) < 2.1'
+
+# can add addtionnal cuts for some bins (first check bin number using tnpEGM --checkBins)
+additionalCuts = { 
+    0 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    1 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    2 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    3 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    4 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    5 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    6 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    7 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    8 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+    9 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45'
+}
+
+#### or remove any additional cut (default)
+additionalCuts = None
+
+#############################################################
+########## fitting params to tune fit by hand if necessary
+#############################################################
+tnpParNomFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[0.5,0.1,5.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[0.5,0.1,5.0]",
+    "acmsP[60.,50.,80.]","betaP[0.05,0.01,0.08]","gammaP[0.1, 0, 1]","peakP[90.0]",
+    "acmsF[60.,50.,80.]","betaF[0.05,0.01,0.08]","gammaF[0.1, 0, 1]","peakF[90.0]",
+    ]
+
+tnpParAltSigFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[1,0.7,6.0]","alphaP[2.0,1.2,3.5]" ,'nP[3,-5,5]',"sigmaP_2[1.5,0.5,6.0]","sosP[1,0.5,5.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[2,0.7,15.0]","alphaF[2.0,1.2,3.5]",'nF[3,-5,5]',"sigmaF_2[2.0,0.5,6.0]","sosF[1,0.5,5.0]",
+    "acmsP[60.,50.,75.]","betaP[0.04,0.01,0.06]","gammaP[0.1, 0.005, 1]","peakP[90.0]",
+    "acmsF[60.,50.,75.]","betaF[0.04,0.01,0.06]","gammaF[0.1, 0.005, 1]","peakF[90.0]",
+    ]
+     
+tnpParAltBkgFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[0.5,0.1,5.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[0.5,0.1,5.0]",
+    "alphaP[0.,-5.,5.]",
+    "alphaF[0.,-5.,5.]",
+    ]
+        
